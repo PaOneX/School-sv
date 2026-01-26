213fa8fd12df22e6e2f259d5be28650c89055848
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-01-26 22:01:15 +0530

    creating studentDTO and StudentEntity

---
+++ src/main/java/edu/icet/entity/SubjectEntity.java
@@ -0,0 +1,23 @@
+package edu.icet.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class SubjectEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String subCode;
+}
